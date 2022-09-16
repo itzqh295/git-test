676acae4c59bc4f7381780505973a4c0ae62a056
itzqh <itzqh@295.com>
2022-09-16 09:37:37 +0800

    2.0--commit

--- src/main/java/com/itzqh/git_test.java
+++ src/main/java/com/itzqh/git_test.java
@@ -3,5 +3,6 @@ package com.itzqh;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello.git");
+        System.out.println("hello.git2");
     }
 }
