6e6bb0d7e4cf636133029a37f781cba2ec8948f4
itzqh <itzqh@295.com>
2022-09-16 09:47:29 +0800

    hot-fix commit

--- src/main/java/com/itzqh/git_test.java
+++ src/main/java/com/itzqh/git_test.java
@@ -2,8 +2,9 @@ package com.itzqh;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello.git");
+        System.out.println("hello.git1");
         System.out.println("hello.git2");
         System.out.println("hello.git3");
+        System.out.println("hello.git4");
     }
 }
