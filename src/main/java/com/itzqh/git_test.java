3d83666e571b0279857d0ba4cb7d142a17178b48
itzqh295 <113521417+itzqh295@users.noreply.github.com>
2022-09-16 11:21:48 +0800

    pull commit

--- src/main/java/com/itzqh/git_test.java
+++ src/main/java/com/itzqh/git_test.java
@@ -4,7 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello.git");
         System.out.println("push test!");
-
-
+        System.out.println("pull test!");
     }
 }
