8c30ce8117021866bf78726ec369220d79474e06
itzqh <itzqh@295.com>
2022-09-16 10:48:42 +0800

    push commit

--- src/main/java/com/itzqh/git_test.java
+++ src/main/java/com/itzqh/git_test.java
@@ -2,12 +2,9 @@ package com.itzqh;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello.git1");
-        System.out.println("hello.git2");
-        System.out.println("hello.git3");
-        System.out.println("hello.git4");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test02!");
+        System.out.println("hello.git");
+        System.out.println("push test!");
+
 
     }
 }
